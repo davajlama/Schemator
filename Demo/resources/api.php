d83e9e2d4a19a64af9f0831889ce4c1c7675135a
David Bittner <bittner@zaslat.cz>
2023-02-20 17:18:42 +0100

    fixies

--- Demo/resources/api.php
+++ Demo/resources/api.php
@@ -10,6 +10,7 @@ $api->info()->title('Example documentation');
 $api->info()->description((string) file_get_contents(__DIR__ . '/description.md'));
 $api->info()->version('1.0.0');
 
+Partition::apply($api, require_once __DIR__ . '/api/recent-sales.php');
 Partition::apply($api, require_once __DIR__ . '/api/user/integration.php');
 
 return $api;
