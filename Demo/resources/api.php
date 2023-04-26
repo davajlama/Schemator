a1545484ac3d83540980a542b252145786b7cab3
David Bittner <bittner@zaslat.cz>
2023-04-26 13:52:55 +0200

    fixies

--- Demo/resources/api.php
+++ Demo/resources/api.php
@@ -10,6 +10,12 @@ $api->info()->title('Example documentation');
 $api->info()->description((string) file_get_contents(__DIR__ . '/description.md'));
 $api->info()->version('1.0.0');
 
-Partition::apply($api, require_once __DIR__ . '/book-store.php');
+Partition::apply($api, Partition::create(function(Api $api) {
+    Partition::apply($api, require_once __DIR__ . '/book-store/product.php');
+    Partition::apply($api, require_once __DIR__ . '/book-store/author.php');
+    Partition::apply($api, require_once __DIR__ . '/book-store/attribute.php');
+
+    Partition::apply($api, require_once __DIR__ . '/book-store/manage.php');
+}));
 
 return $api;
