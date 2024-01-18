55547ad266da689ca05aac0db7aee14655a0352e
David Bittner <bittner@zaslat.cz>
2024-01-18 08:59:05 +0100

    fixies

---
+++ SanitizerAttributes/src/ReflectionVariable.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Davajlama\Schemator\SanitizerAttributes;
+
+use ReflectionAttribute;
+use ReflectionParameter;
+use ReflectionProperty;
+use ReflectionType;
+
+final class ReflectionVariable
+{
+    private ReflectionProperty|ReflectionParameter $property;
+
+    public function __construct(ReflectionParameter|ReflectionProperty $property)
+    {
+        $this->property = $property;
+    }
+
+    public function getName(): string
+    {
+        return $this->property->getName();
+    }
+
+    /**
+     * @return ReflectionAttribute<object>[]
+     */
+    public function getAttributes(): array
+    {
+        return $this->property->getAttributes();
+    }
+
+    public function getType(): ReflectionType|null
+    {
+        return $this->property->getType();
+    }
+}
