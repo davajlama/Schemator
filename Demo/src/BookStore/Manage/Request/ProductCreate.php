e2f85a456ce9bf0c2a6a1d1aa1f8455ab0ca6e4f
David Bittner <bittner@zaslat.cz.>
2025-06-11 09:40:31 +0200

    Add Url rule to json schema

--- Demo/src/BookStore/Manage/Request/ProductCreate.php
+++ Demo/src/BookStore/Manage/Request/ProductCreate.php
@@ -7,12 +7,14 @@ namespace Davajlama\Schemator\Demo\BookStore\Manage\Request;
 use Davajlama\Schemator\SchemaAttributes\Attribute\MaxLength;
 use Davajlama\Schemator\SchemaAttributes\Attribute\MinLength;
 use Davajlama\Schemator\SchemaAttributes\Attribute\RequiredAll;
+use Davajlama\Schemator\SchemaAttributes\Attribute\Url;
 
 #[RequiredAll]
 final class ProductCreate
 {
     public function __construct(
         #[MinLength(3)] #[MaxLength(255)] public string $name,
+        #[Url] public string $url,
     ) {
     }
 }
--- JsonSchema/src/Resolver/FormatResolver.php
+++ JsonSchema/src/Resolver/FormatResolver.php
@@ -7,16 +7,22 @@ namespace Davajlama\Schemator\JsonSchema\Resolver;
 use Davajlama\Schemator\JsonSchema\Definition;
 use Davajlama\Schemator\Schema\RuleInterface;
 use Davajlama\Schemator\Schema\Rules\Email;
+use Davajlama\Schemator\Schema\Rules\Url;
 
 final class FormatResolver implements ResolverInterface
 {
     public function support(RuleInterface $rule): bool
     {
-        return $rule instanceof Email;
+        return $rule instanceof Email
+            || $rule instanceof Url;
     }
 
     public function resolve(Definition $definition, RuleInterface $rule): void
     {
-        $definition->setFormat('email');
+        if ($rule instanceof Email) {
+            $definition->setFormat('email');
+        } elseif ($rule instanceof Url) {
+            $definition->setFormat('uri');
+        }
     }
 }
